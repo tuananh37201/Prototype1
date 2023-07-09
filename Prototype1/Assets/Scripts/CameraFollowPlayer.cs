8d27f7857a2b2d34f02dc7c406f8495e871f789f
tuananh37201 <tuananh37201@gmail.com>
2023-07-09 23:20:07 +0700

    1.3

---
+++ Prototype1/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offSet = new Vector3(0, 4, -7);
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offSet;
+    }
+}
