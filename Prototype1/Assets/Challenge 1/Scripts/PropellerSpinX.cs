85c032a6aa0fc77b863ee22b50ae69a9b8d36206
tuananh37201 <tuananh37201@gmail.com>
2023-07-10 08:32:06 +0700

    Challenge 1

---
+++ Prototype1/Assets/Challenge 1/Scripts/PropellerSpinX.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpinX : MonoBehaviour
+{
+    public float spinSpeed = 300;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
+    }
+}
