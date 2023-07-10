86cfdefda9ed607373d4cfb3de0b3c85b7a94035
tuananh37201 <tuananh37201@gmail.com>
2023-07-10 13:25:10 +0700

    Bonus Features
    https://connect-prd-cdn.unity.com/20210504/93a938b7-ee56-488f-b0b3-8b956c0a31fd/Unit%201%20-%20Bonus%20Features%20Solutions.pdf?_ga=2.199681006.1186801097.1620052249-59568313.1601905412

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
     private float turnSpeed = 25;
     private float horizontalInput;
     private float verticalInput;
+
+    public Camera mainCamera, firstPersonCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -23,5 +25,12 @@ public class PlayerController : MonoBehaviour
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+        // Camera switch
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            firstPersonCamera.enabled = !firstPersonCamera.enabled;
+        }
     }
 }
